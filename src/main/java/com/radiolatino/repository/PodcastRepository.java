61881c93e489203e76535e3ec97f78a6ba081b87
IsaRB <romerobellorini@gmail.com>
2025-04-24 01:13:11 +0200

    Added new changes in PodcastRespository

--- src/main/java/com/radiolatino/repository/PodcastRepository.java
+++ src/main/java/com/radiolatino/repository/PodcastRepository.java
@@ -1,12 +1,42 @@
 package com.radiolatino.repository;
 
 import com.radiolatino.model.Podcast;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 
 import java.util.List;
 
-@Repository
-public interface PodcastRepository extends JpaRepository<Podcast, Long> {
-    List<Podcast> findByGeneroNombre(String nombreGenero);
+@Stateless
+public class PodcastRepository {
+
+    @PersistenceContext(unitName = "defaultPU")
+    private EntityManager em;
+
+    public List<Podcast> findByGeneroNombre(String nombreGenero) {
+        return em.createQuery("SELECT p FROM Podcast p WHERE p.genero.nombre = :nombreGenero", Podcast.class)
+                .setParameter("nombreGenero", nombreGenero)
+                .getResultList();
+    }
+
+    public Podcast findById(Long id) {
+        return em.find(Podcast.class, id);
+    }
+
+    public void save(Podcast podcast) {
+        em.persist(podcast);
+    }
+
+    public void update(Podcast podcast) {
+        em.merge(podcast);
+    }
+
+    public void delete(Long id) {
+        Podcast podcast = findById(id);
+        if (podcast != null) {
+            em.remove(podcast);
+        }
+    }
 }
+
