35912f69fe02cb0a6587a0d2778cf5801bb25c8c
IsaRB <romerobellorini@gmail.com>
2025-04-24 01:00:19 +0200

    Added new code in CancionRepository

--- src/main/java/com/radiolatino/repository/CancionRepository.java
+++ src/main/java/com/radiolatino/repository/CancionRepository.java
@@ -1,12 +1,38 @@
 package com.radiolatino.repository;
 
 import com.radiolatino.model.Cancion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 import java.util.List;
 
-@Repository
-public interface CancionRepository extends JpaRepository<Cancion, Long> {
-    List<Cancion> findByGeneroNombre(String nombreGenero);
+@Stateless
+public class CancionRepository {
+
+    @PersistenceContext(unitName = "radiolatinoPU")
+    private EntityManager em;
+
+    public List<Cancion> findByGeneroNombre(String nombreGenero) {
+        return em.createQuery(
+                        "SELECT c FROM Cancion c WHERE c.genero.nombre = :nombreGenero", Cancion.class)
+                .setParameter("nombreGenero", nombreGenero)
+                .getResultList();
+    }
+
+    public Cancion findById(Long id) {
+        return em.find(Cancion.class, id);
+    }
+
+    public void save(Cancion cancion) {
+        em.persist(cancion);
+    }
+
+    public void update(Cancion cancion) {
+        em.merge(cancion);
+    }
+
+    public void delete(Cancion cancion) {
+        em.remove(em.contains(cancion) ? cancion : em.merge(cancion));
+    }
 }
