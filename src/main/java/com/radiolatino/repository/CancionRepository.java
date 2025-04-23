805d280876b2ffc264ba171cfb6979a0ae2b91ba
IsaRB <romerobellorini@gmail.com>
2025-04-24 01:35:19 +0200

    Added new code in CancionRepository(CxI) + CancionService

--- src/main/java/com/radiolatino/repository/CancionRepository.java
+++ src/main/java/com/radiolatino/repository/CancionRepository.java
@@ -1,38 +1,14 @@
 package com.radiolatino.repository;
 
 import com.radiolatino.model.Cancion;
-import jakarta.ejb.Stateless;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-
 import java.util.List;
+import java.util.Optional;
 
-@Stateless
-public class CancionRepository {
-
-    @PersistenceContext(unitName = "radiolatinoPU")
-    private EntityManager em;
-
-    public List<Cancion> findByGeneroNombre(String nombreGenero) {
-        return em.createQuery(
-                        "SELECT c FROM Cancion c WHERE c.genero.nombre = :nombreGenero", Cancion.class)
-                .setParameter("nombreGenero", nombreGenero)
-                .getResultList();
-    }
-
-    public Cancion findById(Long id) {
-        return em.find(Cancion.class, id);
-    }
-
-    public void save(Cancion cancion) {
-        em.persist(cancion);
-    }
-
-    public void update(Cancion cancion) {
-        em.merge(cancion);
-    }
-
-    public void delete(Cancion cancion) {
-        em.remove(em.contains(cancion) ? cancion : em.merge(cancion));
-    }
+public interface CancionRepository {
+    List<Cancion> findAll();
+    Optional<Cancion> findById(Long id);
+    Cancion save(Cancion cancion);
+    void deleteById(Long id);
+    List<Cancion> findByGeneroNombre(String nombreGenero);
 }
+
