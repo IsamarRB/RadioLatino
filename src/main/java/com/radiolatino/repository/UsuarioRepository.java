831377ca85945188bdc9f50fafd841fcecb3aae0
IsaRB <romerobellorini@gmail.com>
2025-04-24 01:27:35 +0200

    Added new code in UsuarioRepository

--- src/main/java/com/radiolatino/repository/UsuarioRepository.java
+++ src/main/java/com/radiolatino/repository/UsuarioRepository.java
@@ -1,11 +1,45 @@
 package com.radiolatino.repository;
 
-
 import com.radiolatino.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByCorreo(String correo, String password);
+
+@Stateless
+public class UsuarioRepository {
+
+    @PersistenceContext(unitName = "radiolatinoPU")
+    private EntityManager em;
+
+    public Usuario findByCorreoAndPassword(String correo, String password) {
+        TypedQuery<Usuario> query = em.createQuery(
+                "SELECT u FROM Usuario u WHERE u.correo = :correo AND u.password = :password",
+                Usuario.class
+        );
+        query.setParameter("correo", correo);
+        query.setParameter("password", password);
+
+        return query.getResultStream().findFirst().orElse(null);
+    }
+
+    public void save(Usuario usuario) {
+        em.persist(usuario);
+    }
+
+    public Usuario find(Long id) {
+        return em.find(Usuario.class, id);
+    }
+
+    public Usuario update(Usuario usuario) {
+        return em.merge(usuario);
+    }
+
+    public void delete(Long id) {
+        Usuario usuario = em.find(Usuario.class, id);
+        if (usuario != null) {
+            em.remove(usuario);
+        }
+    }
 }
